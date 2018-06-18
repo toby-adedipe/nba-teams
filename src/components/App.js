13ef3230c58edd2baa60ab0795295b5b1d3c3ae0
toby-adedipe <toby_adedipe@yahoo.com>
2018-06-18 01:08:06 +0100

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+// COMPONENTS
+import Header from './header';
+import Footer from './footer';
+import Home from './home';
+
+
+class App extends Component{
+    render(){
+        return (
+            <BrowserRouter>
+                <div>
+                    <Header />
+                    <Route exact path="/" component={Home} />
+                    <Footer />
+                </div>
+            </BrowserRouter>
+        )
+    }
+}
+
+export default App
