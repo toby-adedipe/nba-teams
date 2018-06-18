001ea1fdef7c77264130e40e722ea316c0740902
toby-adedipe <toby_adedipe@yahoo.com>
2018-06-18 22:13:26 +0100

    added react-slick

---
+++ src/components/featured.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Slider from 'react-slick';
+
+const Featured = (props)=>{
+    console.log(props)
+    return(
+        <div> slider </div>
+    )
+}
+
+export default Featured;
